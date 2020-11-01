99c2073563c35ee1fead5d35e92815109cf92ed9
AliNijat <67079299+AliNijat@users.noreply.github.com>
2020-11-01 11:55:18 -0500

    Update Flash.java
    I added a line code

--- src/main/java/Flash.java
+++ src/main/java/Flash.java
@@ -2,6 +2,8 @@ public class Flash {
 
     public static void main(String[] args) {
         System.out.println("flash run very fast");
+        
+        // I want test it, it works or no
 
     }
 }
